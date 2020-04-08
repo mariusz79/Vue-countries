349c56ee3c65b16ae21ae97458888d3eb3318195
mariusz79 <mpawlowic@gmail.com>
2020-04-08 17:09:55 +0100

    Connect to API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,27 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="get()">Get</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  methods: {
+    get() {
+      axios.get('https://restcountries.eu/rest/v2/alpha/col')
+        .then((response) => {
+          console.log(response);
+        });
+    },
   },
 };
 </script>
