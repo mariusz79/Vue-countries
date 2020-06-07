7788fe6b3d37505dc366578ff10c269e29bc214a
mariusz79 <mpawlowic@gmail.com>
2020-06-08 00:05:07 +0100

    Move dark theme switcher to separate component

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="mode" v-on:click='darkMode'>
+            <img :src="darkOn? moonBlack : moonLine" alt="moon icon">
+            <p class="mode-text">Dark Mode</p>
+          </div>
+</template>
+<script>
+export default {
+  name: 'ToggleTheme',
+  data() {
+    return {
+      darkOn: false,
+      moonLine: 'https://mariusz-ecommerce.s3-eu-west-1.amazonaws.com/static/Vue/moon-line.svg',
+      moonBlack: 'https://mariusz-ecommerce.s3-eu-west-1.amazonaws.com/static/Vue/moon-black.svg',
+    };
+  },
+  methods: {
+    darkMode() {
+      this.darkOn = !this.darkOn;
+      this.$emit('darkMode');
+    },
+  },
+};
+</script>
+<style lang="scss">
+</style>
